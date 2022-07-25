4fadcbc3e2d5b6f2692614c9ae0c9f67c6473831
Leandro <lecorvalan2@gmail.com>
2022-07-25 03:01:12 -0300

    Creando Sidebar component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,19 @@
+import Image from "next/image";
+
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <section className="fixed top-0 z-40 flex flex-col p-4 items-center bg-black w-[90px] h-screen space-y-8">
+      <Image
+        src="https://rb.gy/xkacau"
+        width={56}
+        height={56}
+        objectFit="contain"
+        alt="logo"
+      />
+      <div className="text-white">
+        hello world
+      </div>
+    </section>
   )
 }
 
