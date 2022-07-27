24332d703e9642d23ea58fcc48878070e978f93d
Leandro <lecorvalan2@gmail.com>
2022-07-26 23:18:31 -0300

    Creando Login page y añadiendole ServerSideProps

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,7 +1,44 @@
-function Signin() {
+import { getProviders, useSession } from "next-auth/react"
+import Head from "next/head"
+import Image from "next/image"
+
+function Signin({providers}) {
+    const {data: session} = useSession();
+
   return (
-    <div>Signin</div>
+    <div>
+        <Head>
+            <title>Login - Dashboard</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <Image
+            src="https://rb.gy/y9mwtb"
+            height={250}
+            width={600}
+            objectFit="contain"
+            className="animate-pulse"
+            alt="logo"
+        />
+        {Object.values(providers).map((provider) => (
+            <div key={provider.id}>
+                <button 
+                    className="text-white py-4 px-6 rounded-full bg-[#1db954] transition duration-300 ease-out border border-transparent uppercase font-bold text-xs md:text-base tracking-wider hover:scale-105 hover:bg-[#0db146]"
+                    onClick={() => signIn(provider.id)}
+                >
+                    Sign in with {provider.name}
+                </button>
+            </div>
+        ))}
+    </div>
   )
 }
 
 export default Signin
+
+export async function getServerSideProps() {
+    const providers = await getProviders();
+    return {
+      props: { providers },
+    };
+}
