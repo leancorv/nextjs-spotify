1828f16665dbda5e41971ddddd191506f4e0f02d
Leandro <lecorvalan2@gmail.com>
2022-07-28 17:22:19 -0300

    Body - Haciendo map de los resultados

--- components/Body.js
+++ components/Body.js
@@ -62,7 +62,19 @@ function Body({ spotifyApi }) {
     <section className="bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5">
       <Search search={search} setSearch={setSearch} />
       <div className="grid overflow-y-scroll scrollbar-hide h-96 py-4 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8 p-4">
-
+        {searchResults.length === 0 ? newReleases.slice(0,4).map((track) => (
+          <Poster 
+            key={track.id} 
+            track={track} 
+            // chooseTrack={chooseTrack}
+          />
+        )) : searchResults.slice(0,4).map((track) => (
+          <Poster 
+            key={track.id} 
+            track={track} 
+            // chooseTrack={chooseTrack}
+          />
+        ))}
       </div>
     </section>
   )
