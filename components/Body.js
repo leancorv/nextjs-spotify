a3a3fccdf8786dfd91f909d6ac887478671ebb4c
Leandro <lecorvalan2@gmail.com>
2022-07-28 03:22:38 -0300

    Body - SpotifyWebApi AccessToken

--- components/Body.js
+++ components/Body.js
@@ -2,7 +2,7 @@ import { useSession } from "next-auth/react"
 import { useEffect, useState } from "react"
 import Search from "./Search"
 
-function Body() {
+function Body({ spotifyApi }) {
   const {data: session} = useSession();
   const { accessToken } = session;
   const [search, setSearch] = useState("");
@@ -11,7 +11,9 @@ function Body() {
 
   useEffect(() => {
     if (!accessToken) return;
-  }, []);
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+  
 
   return (
     <section className="bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5">
