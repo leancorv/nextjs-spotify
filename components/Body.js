07d2d86f6fe78fbdc34c9e631774a627f2430ed5
Leandro <lecorvalan2@gmail.com>
2022-07-29 03:28:16 -0300

    Body - Creando track container

--- components/Body.js
+++ components/Body.js
@@ -2,6 +2,7 @@ import { useSession } from "next-auth/react"
 import { useEffect, useState } from "react"
 import Poster from "./Poster";
 import Search from "./Search"
+import Track from "./Track";
 
 function Body({ spotifyApi, chooseTrack }) {
   const {data: session} = useSession();
@@ -98,8 +99,33 @@ function Body({ spotifyApi, chooseTrack }) {
           </button>
         </div>
 
-        
-
+        {/* Tracks */}
+        <div>
+          <h2 className="text-white font-bold mb-3">
+            {searchResults.length === 0 ? "New Releases" : "Tracks"}
+          </h2>
+          <div className="space-y-3 border-2 border-[#262626] rounded-2xl p-3 bg-[#0D0D0D] overflow-y-scroll h-[1000px] md:h-96 scrollbar-thin scrollbar-thumb-gray-600 scrollbar-thumb-rounded hover:scrollbar-thumb-gray-500 w-[830px]">
+            {searchResults.length === 0
+              ? newReleases
+                  .slice(4, newReleases.length)
+                  .map((track) => (
+                    <Track
+                      key={track.id}
+                      track={track}
+                      chooseTrack={chooseTrack}
+                    />
+                  ))
+              : searchResults
+                  .slice(4, searchResults.length)
+                  .map((track) => (
+                    <Track
+                      key={track.id}
+                      track={track}
+                      chooseTrack={chooseTrack}
+                    />
+                  ))}
+          </div>          
+        </div>
       </div>
     </section>
   )
