6d075622c58d921d9992046befc4eeb3dae46db1
Leandro <lecorvalan2@gmail.com>
2022-07-29 15:39:49 -0300

    Right - Maquetando settings icons

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -21,7 +21,7 @@ function Dashboard() {
     <main className="flex min-h-screen min-w-max bg-black lg:pb-24">
         <Sidebar />
         <Body spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
-        <Right />
+        <Right spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
     </main>
   )
 }
--- components/Right.js
+++ components/Right.js
@@ -1,6 +1,21 @@
+import { HiOutlineShieldCheck } from "react-icons/hi";
+import { MdOutlineSettings } from "react-icons/md";
+import { BiBell } from "react-icons/bi";
+import { ViewGridIcon } from "@heroicons/react/solid";
+
 function Right() {
   return (
-    <div>Right</div>
+    <section className="p-4 space-y-8 pr-8">
+      <div className="flex space-x-2 items-center justify-between">
+        {/* Icons */}
+        <div className="flex items-center space-x-4 border-2 border-[#262626] rounded-full h-12 py-3 px-4">
+          <HiOutlineShieldCheck className="text-[#ccc] text-xl" />
+          <MdOutlineSettings className="text-[#ccc] text-xl" />
+          <BiBell className="text-[#ccc] text-xl" />
+        </div>
+        {/* Profile */}
+      </div>
+    </section>
   )
 }
 
