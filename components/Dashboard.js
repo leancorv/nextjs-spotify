0d50a2aaa8cbf8046ce009e855bf577fb6fc5187
Leandro <lecorvalan2@gmail.com>
2022-07-30 02:40:37 -0300

    Dashboard - Creando player container

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -4,6 +4,9 @@ import { playingTrackState } from "../atoms/playerAtom";
 import Body from "./Body"
 import Right from "./Right"
 import Sidebar from "./Sidebar"
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import Player from "./Player";
 
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.SPOTIFY_CLIENT_ID,
@@ -12,16 +15,29 @@ const spotifyApi = new SpotifyWebApi({
 
 
 function Dashboard() {
+    const { data: session } = useSession();
+    const accessToken = session?.accessToken;
+
     const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+    const [showPlayer, setShowPlayer] = useState(false);
+
+    useEffect(() => {
+        setShowPlayer(true);
+    }, []);
 
     const chooseTrack = (track) => {
         setPlayingTrack(track);
     }
+
     return (
     <main className="flex min-h-screen min-w-max bg-black lg:pb-24">
         <Sidebar />
         <Body spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
         <Right spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
+
+        <div className="fixed bottom-0 left-0 right-0 z-50">
+            <Player accessToken={accessToken} trackUri={playingTrack.uri} />
+        </div>
     </main>
   )
 }
